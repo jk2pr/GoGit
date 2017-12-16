ff18486a6f6376dcceadd0db0f3bec74eeb2dcfa
jitendra <jk2pr@yahoo.co.in>
2017-12-16 09:17:12 +0530

    Initial Commit

---
+++ SimpleIosApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleIosApp
+//
+//  Created by Jitendra on 16/12/2017.
+//  Copyright © 2017 Jitendra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
