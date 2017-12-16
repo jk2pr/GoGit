1cbf54af3362a51664d8c1d8363570d9680d80f7
jitendra <jk2pr@yahoo.co.in>
2017-12-16 18:18:57 +0530

    added click event on Button

--- SimpleIosApp/ViewController.swift
+++ SimpleIosApp/ViewController.swift
@@ -10,7 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
-   
+    @IBOutlet weak var lable: UILabel!
+    
+    @IBOutlet weak var button: UIButton!
+    
+    @IBAction func onClick(_ sender: UIButton) {
+        lable.text="updated by button"
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
