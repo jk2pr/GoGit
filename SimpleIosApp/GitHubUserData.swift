8b909c3a371160da2d686520689f2d361a3c3a92
jitendra <jk2pr@yahoo.co.in>
2017-12-17 21:56:31 +0530

    added alamofire and SwifyJson library through carthage, Implemented tableview with GitHub user data

---
+++ SimpleIosApp/GitHubUserData.swift
@@ -0,0 +1,50 @@
+//
+//  GitHubUserData.swift
+//  SimpleIosApp
+//
+//  Created by Jitendra on 17/12/2017.
+//  Copyright © 2017 Jitendra. All rights reserved.
+//
+
+import Foundation
+class GitHubeUserData{
+    var uid: Int?;
+    var login: String?;
+    var avatar_url: String?;
+    var id: Int?;
+    var url: String?;
+    var html_url: String?;
+    var followers_url: String?;
+    var following_url: String?;
+    var starred_url: String?;
+    var gists_url: String?;
+    var type: String?;
+    var score: Double;
+    init(uid: Int?,
+    login: String?,
+     avatar_url: String?,
+     id: Int?,
+     url: String?,
+     html_url: String?,
+     followers_url: String?,
+     following_url: String?,
+     starred_url: String?,
+     gists_url: String?,
+     type: String?,
+     score: Double)
+    {
+        self.uid=uid
+        self.login=login
+        self.avatar_url=avatar_url
+        self.id=id
+        self.url=url
+        self.html_url=html_url
+        self.followers_url=followers_url
+        self.following_url=following_url
+        self.starred_url=starred_url
+        self.gists_url=gists_url
+        self.type=type
+        self.score=score
+    }
+
+}
--- SimpleIosApp/ViewController.swift
+++ SimpleIosApp/ViewController.swift
@@ -7,20 +7,75 @@
 //
 
 import UIKit
+import Alamofire
+import AlamofireSwiftyJSON
+import SwiftyJSON
 
-class ViewController: UIViewController {
+class GitHubUserCell: UITableViewCell {
+    
+    @IBOutlet weak var imageview: UIImageView!
+    
+    @IBOutlet weak var namelabel: UILabel!
+    @IBOutlet weak var urlLabel: UILabel!
+    
+}
 
-    @IBOutlet weak var lable: UILabel!
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var tableView: UITableView!
     
-    @IBOutlet weak var button: UIButton!
+  private  var arrRes:[GitHubeUserData]=[]
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
     
-    @IBAction func onClick(_ sender: UIButton) {
-        lable.text="updated by button"
+    {
+        return arrRes.count
+    }
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! GitHubUserCell
+        let data=arrRes[indexPath.row]
+        cell.urlLabel.text=data.login
+      
+        return cell
     }
     override func viewDidLoad() {
         super.viewDidLoad()
+        Alamofire.request("https://api.github.com/search/users?q=Kotlin").responseJSON { (responseData) -> Void in
+            if((responseData.result.value) != nil) {
+                let swiftyJsonVar = JSON(responseData.result.value!)
+                print(swiftyJsonVar)
+                let items = swiftyJsonVar["items"].arrayValue
+                    for  item  in items
+                    {
+                        let uid = item["uid"].int
+                        let login = item["login"].string
+                        let avatar_url = item["avatar_url"].string
+                        let id = item["id"].int
+                        let url = item["url"].string
+                        let html_url = item["html_url"].string
+                        let followers_url = item["followers_url"].string
+                        let following_url = item["following_url"].string
+                        let starred_url = item["starred_url"].string
+                        let gists_url = item["gist_url"].string
+                        let type = item["type"].string
+                        let score = item["score"].double
+                       
+                        
+                        let gitHubUser=GitHubeUserData(uid: uid,login: login,avatar_url: avatar_url,id: id,url: url,html_url: html_url,followers_url: followers_url,following_url: following_url,starred_url: starred_url,gists_url: gists_url,type: type,score: score!)
+
+                        self.arrRes.append(gitHubUser)
+                    }
+                    
+                }
+                   // self.arrRes = items as! [String:AnyObject]
+                }
+                if self.arrRes.count > 0 {
+                  //  print(self.arrRes)
+                  self.tableView.reloadData()
+                }
+            }
+        }
         // Do any additional setup after loading the view, typically from a nib.
-    }
+
   
-}
+
 
