0850356dc299472eda59bdbf5dd84c1862e1437c
jitendra <jk2pr@yahoo.co.in>
2018-01-24 05:20:21 +0530

    added GitHub login

--- SimpleIosApp/WebAlertViewController.swift
+++ SimpleIosApp/WebAlertViewController.swift
@@ -10,26 +10,47 @@ import Foundation
 import UIKit
 import WebKit
 
-class WebAlertViewController: ViewController,WKUIDelegate{
+class WebAlertViewController: ViewController, WKNavigationDelegate{
     
     @IBOutlet weak var webview: WKWebView!
+     let clientId="N/A"
+     let secretId="N/A"
+     let githubLoginUrl="https://www.github.com/login/oauth/authorize"
+    
+    
     private let REDIRECT_URL_CALLBACK = "https://daggerrxkotlin.firebaseapp.com/__/auth/handler"
     override func viewDidLoad() {
         self.view.backgroundColor = UIColor(white: 0.5, alpha: 0.4)
-        webview.uiDelegate=self
+        webview.navigationDelegate = self
        
         
     }
-    func webView(_ webView: UIWebView, shouldStartLoadWith request: URLRequest, navigationType: UIWebViewNavigationType) -> Bool {
-        
-        if request.url?.absoluteString == "https://www.google.com" {
-            return false
+    func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!){
+       // print(webView.url ?? "google.co.in")
+        if(webView.url?.absoluteString.starts(with:REDIRECT_URL_CALLBACK))!{
+            self.dismiss(animated: true, completion: nil)
         }
-        return true
+        print(webView.url?.absoluteString as Any)
+        print(REDIRECT_URL_CALLBACK)
+
     }
+ 
     override func viewDidAppear(_ animated: Bool) {
-        let urlReq = URLRequest(url: URL(string: "http://www.google.co.in")!)
+        
+        let u = self.githubLoginUrl + "?scope=user:email user:follow public_repo &client_id=" as String
+        let v = clientId + "&secretId=" as String
+        let completeUrl = u +
+            v+secretId + "&state=" + "123"
+        
+        
+        print(completeUrl)
+        //completeUrl.stringByAddingPercentEncodingWithAllowedCharacters()
+        //let csCopy = CharacterSet(bitmapRepresentation:
+          //  CharacterSet.urlPathAllowed.bitmapRepresentation)
+        let urlReq = URLRequest(url: URL(string: completeUrl.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed)!)!)
+        print(urlReq.url as Any)
         webview.load(urlReq)
+       
         
     }
     override func viewDidDisappear(_ animated: Bool) {
