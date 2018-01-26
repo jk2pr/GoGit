09d3e41ec791143ffae6d3de9ac3fd04c90dacda
jitendra <jk2pr@yahoo.co.in>
2018-01-26 07:36:48 +0530

    Added firebase auth

--- SimpleIosApp/SplashViewController.swift
+++ SimpleIosApp/SplashViewController.swift
@@ -9,15 +9,17 @@
 
 import UIKit
 import WebKit
+import Realm
+import FirebaseAuth
+
 
 class SplashViewController: UIViewController{
     @IBOutlet weak var logo: UIImageView!
-    
-   
     @IBOutlet weak var skip: UIButton!
     @IBOutlet weak var loginWithGitHub: UIButton!
-
     
+    
+
     
     @IBAction func onSkip(_ sender: UIButton) {
         if(!self.logo.isAnimating)
@@ -26,37 +28,23 @@ class SplashViewController: UIViewController{
         
     }
     @IBAction func onLoginButtonClick(_ sender: UIButton) {
+        if(!self.logo.isAnimating){
+        let alertViewController = self.storyboard?.instantiateViewController(withIdentifier: "webalertviewcontroller") as!  WebAlertViewController
+        self.present(alertViewController, animated: true, completion: nil)
+        }
     }
     override func viewDidAppear(_ animated: Bool) {
-        
         self.logo.transform = CGAffineTransform(translationX: 0, y: logo.frame.origin.y+200)
             .concatenating(CGAffineTransform(scaleX: 0.5, y: 0.5))
-        
-        
+    
         UIView.animate(withDuration: 5.0, delay: 0.0, usingSpringWithDamping: 0.6, initialSpringVelocity: 1, options: [.curveEaseOut], animations: {
             self.logo.transform = .identity
             self.logo.alpha = 1
             
         }, completion:{
              (value: Bool) in
-            
-            let alertViewController = self.storyboard?.instantiateViewController(withIdentifier: "webalertviewcontroller") as!  WebAlertViewController
-            
-            self.present(alertViewController, animated: true, completion: nil)
-
-            
-//            let alert = UIAlertController(title: "Alert", message: "Message", preferredStyle: UIAlertControllerStyle.alert)
-//            //  alert.addAction(UIAlertAction(title: "Click", style: UIAlertActionStyle.default, handler: nil))
-//            let wkWebView=WKWebView()
-//            let urlReq = URLRequest(url: URL(string: "https://www.google.co.in")!)
-//            wkWebView.load(urlReq)
-//            alert.view.addSubview(wkWebView)
-//            self.present(alert, animated: true, completion: nil)
-//
-            
-        
-            
-        })
+           
+ })
         
     }
 }
--- SimpleIosApp/WebAlertViewController.swift
+++ SimpleIosApp/WebAlertViewController.swift
@@ -9,16 +9,19 @@
 import Foundation
 import UIKit
 import WebKit
+import FirebaseAuth
+import Alamofire
+import SwiftyJSON
 
 class WebAlertViewController: ViewController, WKNavigationDelegate{
     
     @IBOutlet weak var webview: WKWebView!
-     let clientId="N/A"
-     let secretId="N/A"
-     let githubLoginUrl="https://www.github.com/login/oauth/authorize"
+     let clientId=""
+     let secretId=""
+     let githubLoginUrl="https://github.com/login/oauth/authorize"
     
     
-    private let REDIRECT_URL_CALLBACK = "https://daggerrxkotlin.firebaseapp.com/__/auth/handler"
+    private let REDIRECT_URL_CALLBACK = "https://gogit-5a346.firebaseapp.com/__/auth/handler"
     override func viewDidLoad() {
         self.view.backgroundColor = UIColor(white: 0.5, alpha: 0.4)
         webview.navigationDelegate = self
@@ -28,13 +31,71 @@ class WebAlertViewController: ViewController, WKNavigationDelegate{
     func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!){
        // print(webView.url ?? "google.co.in")
         if(webView.url?.absoluteString.starts(with:REDIRECT_URL_CALLBACK))!{
-            self.dismiss(animated: true, completion: nil)
+            let code = getQueryStringParameter(url: (webView.url?.absoluteString)!, param: "code")
+            print("Coode  " + code!)
+            getAccessToken(code: code!)
+            
         }
-        print(webView.url?.absoluteString as Any)
-        print(REDIRECT_URL_CALLBACK)
-
     }
- 
+    func getQueryStringParameter(url: String, param: String) -> String? {
+        guard let url = URLComponents(string: url) else { return nil }
+        return url.queryItems?.first(where: { $0.name == param })?.value
+    }
+    
+    func getAccessToken(code: String){
+       let accessTokenUrl = "https://github.com/login/oauth/access_token"
+       
+        let params = ["client_id": clientId,
+                      "client_secret": secretId,
+                      "redirect_uri": REDIRECT_URL_CALLBACK,
+                      "state": "123",
+                      "code": code]
+        let headers=["Accept": "application/json"]
+      
+        
+        Alamofire.request(accessTokenUrl,
+                          method: .post,
+                          parameters: params,
+                          encoding: JSONEncoding.default,
+                          headers: headers)
+            .validate(contentType: ["application/json"])
+            .responseJSON
+            { (responseData) -> Void in
+              
+                guard responseData.result.isSuccess else {
+                    print("Error while fetching remote rooms: \(String(describing: responseData.result.error))")
+                    return
+                }
+                if((responseData.result.value) != nil) {
+                     let swiftyJsonVar = JSON(responseData.result.value!)
+                    print(swiftyJsonVar)
+                    let  accessToken = swiftyJsonVar["access_token"].string!
+                    let credential = GitHubAuthProvider.credential(withToken: accessToken)
+                    self.signInWithFireBase(credential: credential)
+                   
+                }
+        }
+      
+        
+    }
+    func signInWithFireBase(credential:AuthCredential){
+        Auth.auth().signIn(with: credential) {
+            (user, error) in
+            if let error = error {
+                print(error.localizedDescription)
+                return
+            } else{
+                print("User Signed In sucessfully")
+                
+            }
+            self.dismiss(animated: true, completion: nil)
+            // User is signed in
+            // ...
+        }
+    }
+      
+    
+    
     override func viewDidAppear(_ animated: Bool) {
         
         let u = self.githubLoginUrl + "?scope=user:email user:follow public_repo &client_id=" as String
@@ -43,12 +104,12 @@ class WebAlertViewController: ViewController, WKNavigationDelegate{
             v+secretId + "&state=" + "123"
         
         
-        print(completeUrl)
+       // print(completeUrl)
         //completeUrl.stringByAddingPercentEncodingWithAllowedCharacters()
         //let csCopy = CharacterSet(bitmapRepresentation:
           //  CharacterSet.urlPathAllowed.bitmapRepresentation)
         let urlReq = URLRequest(url: URL(string: completeUrl.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed)!)!)
-        print(urlReq.url as Any)
+      //  print(urlReq.url as Any)
         webview.load(urlReq)
        
         
