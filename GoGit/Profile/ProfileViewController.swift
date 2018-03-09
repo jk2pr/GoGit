ae21da02d1e81717d311fa3898b71f9a9013ac3f
jitendra <jk2praj@gmail.com>
2018-03-09 06:21:12 +0530

    Added image view in tableview header

--- GoGit/Profile/ProfileViewController.swift
+++ GoGit/Profile/ProfileViewController.swift
@@ -50,17 +50,21 @@ class ProfileViewController: UIViewController,UITableViewDelegate, UITableViewDa
     }
     
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-      //  let v=UIView()
-       // v.backgroundColor=UIColor.cyan
-        let segmentedControl=UISegmentedControl(frame:CGRect(x:10,y:5,width:tableView.frame.width,height:60))
+      //
+        let v=UIView()
+        v.backgroundColor=UIColor.cyan
+        let image=UIImageView(frame:CGRect(x:10,y:5,width:tableView.frame.width,height:60))
+        image.backgroundColor=UIColor.yellow
+        let segmentedControl=UISegmentedControl(frame:CGRect(x:10,y:image.frame.height+5,width:tableView.frame.width,height:60))
         segmentedControl.backgroundColor=UIColor.red
        
         segmentedControl.insertSegment(withTitle: "Repository", at: 0, animated: true)
         segmentedControl.insertSegment(withTitle: "Followers", at: 1, animated: true)
         segmentedControl.insertSegment(withTitle: "Followings", at: 2, animated: true)
          segmentedControl.setTitle("Test", forSegmentAt: 0)
-       // v.addSubview(segmentedControl)
-        return segmentedControl
+        v.addSubview(image)
+        v.addSubview(segmentedControl)
+        return v
     }
     func getUserRepository(){
         let accessToken=UserDefaults.standard.value(forKey: "access_token") as! String
