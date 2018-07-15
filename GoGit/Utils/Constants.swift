d93b709eff36bc0bcfb3f8ab2f3a1e800736b338
jitendra <jk2praj@gmail.com>
2018-07-15 15:02:15 +0530

    Changes in Constant file

--- GoGit/Utils/Constants.swift
+++ GoGit/Utils/Constants.swift
@@ -8,6 +8,8 @@
 
 import Foundation
 struct  Constants {
+    
+    
     static let clientId="5db96facac0277a35f64",
     secretId="b62824aa8700b99feb0e576d97130d801b47fe1c",
     githubLoginUrl="https://github.com/login/oauth/authorize",
