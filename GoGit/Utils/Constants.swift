5f95fb1f8a5e972a567546650b7e7615484e61d8
jitendra <jk2praj@gmail.com>
2018-07-15 19:54:15 +0530

    Modified constant file

--- GoGit/Utils/Constants.swift
+++ GoGit/Utils/Constants.swift
@@ -8,9 +8,7 @@
 
 import Foundation
 struct  Constants {
-    
-    
-    static let clientId="5db96facac0277a35f64",
+static let clientId="5db96facac0277a35f64",
     secretId="b62824aa8700b99feb0e576d97130d801b47fe1c",
     githubLoginUrl="https://github.com/login/oauth/authorize",
     REDIRECT_URL_CALLBACK = "https://gogit-5a346.firebaseapp.com/__/auth/handler",
