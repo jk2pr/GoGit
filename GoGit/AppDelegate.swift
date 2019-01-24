4b89ecedca30a9524ef5da56198fc15808aee3c4
Jitendra Prajapati <prajapati@Jitendras-MacBook-Pro.local>
2019-01-24 16:43:03 +0800

    Test commit

--- GoGit/AppDelegate.swift
+++ GoGit/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
+//Today is 24 January
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
         let userDefaults = UserDefaults.standard
